f28287032b59feac7133fa853fe019908dd88fe2
Toktam <ttmohebbi@gmail.com>
2020-10-18 22:52:39 +0000

    add new listing store in database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -162,7 +162,7 @@ const getAllProperties = (options, limit = 10) => {
   JOIN property_reviews ON properties.id = property_id
   `;
 
-  /* following is the options objects 
+  /* following is the options object 
   {
   city,
   owner_id, // doesn't exist but whatever
@@ -185,7 +185,7 @@ const getAllProperties = (options, limit = 10) => {
     queryString += `${length === 0 ? 'WHERE' : ' AND'} owner_id = $${length}`;
   }
 
-  // if minimum and maximum cost is working
+  // if minimum and maximum cost is entered
   if (options.minimum_price_per_night) {
     const min = options.minimum_price_per_night * 100;
     queryParams.push(`${min}`);
@@ -226,7 +226,7 @@ const getAllProperties = (options, limit = 10) => {
   `;
 
   // testing
-  console.log(queryString, queryParams);
+  // console.log(queryString, queryParams);
 
   return pool.query(queryString, queryParams)
     .then(response => response.rows)
@@ -241,10 +241,37 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+// const addProperty = function(property) {
+//   const propertyId = Object.keys(properties).length + 1;
+//   property.id = propertyId;
+//   properties[propertyId] = property;
+//   return Promise.resolve(property);
+// }
+
+
+const addProperty = property => {
+  // console.log(property);
+
+  const queryString = `INSERT INTO properties (
+    owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night,
+    street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;`
+
+  const queryParams = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night,
+  property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms];
+
+  // stretch: what if this order is different because object looping order is not guaranteed
+  // for (const key in property) {
+  //   queryParams.push(property[key]);
+  // }
+  // console.log(queryParams);
+
+  return pool.query(queryString, queryParams)
+    .then(response => response.rows[0])
+    .catch(error => error.stack);
 }
+
+
 exports.addProperty = addProperty;
